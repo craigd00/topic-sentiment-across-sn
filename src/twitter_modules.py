eb16d518adbe1594157adb2a15ef3b517aef8498
Craig James Dick <2378675d@student.gla.ac.uk>
2022-02-26 22:35:23 +0000

    Changed files to include shared modules, before bert run, going to create new branch to work on this (Refers to #34)

--- src/twitter_modules.py
+++ src/twitter_modules.py
@@ -79,11 +79,12 @@ def pos_neg_count(dictionary):
     return total_sentiment
 
 
-def plot_topics(data_points):
+def plot_topics(data_points, filename):
     list_of_topics = ['Facemasks', 'Lockdown', 'PCR', 'Pfizer', 'Quarantine', 'Restrictions', 'Vaccine']
     fig = plt.figure()
     x_point = np.arange(7)
     fig = plt.figure(figsize=(18, 10))
+
     ax = fig.add_axes([0,0,1,1])
     ax.bar(x_point + 0.00, data_points[0], color = 'g', width = 0.25)
     ax.bar(x_point + 0.25, data_points[1], color = 'r', width = 0.25)
@@ -91,9 +92,10 @@ def plot_topics(data_points):
     ax.set_ylabel('Number of posts', fontweight='bold', fontsize=16)
     ax.set_xlabel('Search query', fontweight='bold', fontsize=16)
     ax.set_title("Sentiment amongst Twitter queries", fontweight='bold', fontsize=20)
-    plt.xticks(x_point + 0.25, list_of_topics)
 
+    plt.xticks(x_point + 0.25, list_of_topics)
     ax.legend(labels=['Positive', 'Negative', 'Neutral'])
+    plt.savefig(filename)
 
 def database_as_textblob(db):
     list_of_dicts = []
@@ -159,95 +161,3 @@ def database_as_afinn(db, analyzer):
 
         list_of_dicts += [empty]
     return list_of_dicts
-
-
-def database_as_tweet(db):
-    list_of_dicts = []
-
-    for entry in db.SocialMediaPosts.find():
-        empty = {}
-        empty['tweet'] = entry['tweet']
-
-        list_of_dicts += [empty]
-
-    dataframe = pd.DataFrame(list_of_dicts, columns=['tweet'])
-    return dataframe
-
-
-def bert_preprocess(tweet):
-    new_tweet = []
- 
-    for t in tweet.split(" "):
-        t = '' if t.startswith('@') and len(t) > 1 else t
-        t = 'http' if t.startswith('http') else t
-        new_tweet.append(t)
-    return " ".join(new_tweet)
-
-
-labels=[]
-mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
-with urllib.request.urlopen(mapping_link) as f:
-    html = f.read().decode('utf-8').split("\n")
-    csvreader = csv.reader(html, delimiter='\t')
-labels = [row[1] for row in csvreader if len(row) > 1]
-
-
-model = AutoModelForSequenceClassification.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
-tokenizer = AutoTokenizer.from_pretrained("cardiffnlp/twitter-roberta-base-sentiment", model_max_length=512, padding_side="right")
-    
-model.save_pretrained("cardiffnlp/twitter-roberta-base-sentiment")
-tokenizer.save_pretrained("cardiffnlp/twitter-roberta-base-sentiment", model_max_length=512, padding_side="right")
-
-
-def process_emoji(text):
-    new_text = re.sub(emoji.get_emoji_regexp(), r"", text)
-    return new_text
-
-
-def database_as_bert(df):
-    
-    for t in df:
-        try:
-            t = bert_preprocess(t)
-            encoded_input = tokenizer(t, return_tensors='pt')
-            output = model(**encoded_input)
-        except:
-            t = process_emoji(t)
-            encoded_input = tokenizer(t, max_length=512, truncation=True, return_tensors='pt')
-            output = model(**encoded_input)
-        finally:
-            scores = output[0][0].detach().numpy()
-            scores = softmax(scores)
-            ranking = np.argsort(scores)
-            ranking = ranking[::-1]
-            df["sentiment"] = labels[ranking[0]]
-
-    return df
-
-
-def positive_neg_count_df(df):
-    positive = df[df["sentiment"]=="positive"].count()["sentiment"]
-    negative = df[df["sentiment"]=="negative"].count()["sentiment"]
-    neutral = df[df["sentiment"]=="neutral"].count()["sentiment"]
-
-    pos_perc = (positive/len(df)) * 100
-    neg_perc = (negative/len(df)) * 100
-    neu_perc = (neutral/len(df)) * 100
-
-    sentiment_percent = {"pos_perc": pos_perc, "neg_perc": neg_perc, "neu_perc": neu_perc}
-    return sentiment_percent
-
-
-def sentiment_dpts(f_sent, l_sent, pcr_sent, pf_sent, q_sent, r_sent, v_sent):
-    
-    query_dpts = []
-    positive = []
-    negative = []
-    neutral = []
-
-    for sentiment in f_sent, l_sent, pcr_sent, pf_sent, q_sent, r_sent, v_sent:
-        positive += [sentiment['pos_perc']]
-        negative += [sentiment['neg_perc']]
-        neutral += [sentiment['neu_perc']]
-
-    query_dpts += [positive, negative, neutral]
